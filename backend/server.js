314618ca679ebb5026adaccae4def6401dc2b5dc
Ankurrr08 <ankurpandey2115@gmail.com>
2026-05-18 21:44:25 +0530

    fixed mongodb startup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
@@ -7,11 +8,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/tasks", require("./routes/taskRoutes"));
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-const PORT = process.env.PORT || 8080;
+    console.log("MongoDB Connected");
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    app.use("/api/auth", require("./routes/authRoutes"));
+    app.use("/api/tasks", require("./routes/taskRoutes"));
+
+    const PORT = process.env.PORT || 8080;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectDB();
