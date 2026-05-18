f1884180f7a026031fb6f17460c065262e6e2dd9
Ankurrr08 <ankurpandey2115@gmail.com>
2026-05-18 14:15:09 +0530

    fixed api base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "task-manager-app-production-8089.up.railway.app",
+  baseURL: "https://task-manager-app-production-8089.up.railway.app/api",
 });
 
 export default API;
